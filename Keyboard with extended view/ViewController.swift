8c5e1bd7156fd61ca56b847959e4d4c3ec88bc5c
Decha Moungsri <mamegomaday@gmail.com>
2015-07-05 20:42:20 +0700

    Initial Commit

---
+++ Keyboard with extended view/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Keyboard with extended view
+//
+//  Created by Decha Moungsri on 7/5/2558 BE.
+//  Copyright (c) 2558 Decha Moungsri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
